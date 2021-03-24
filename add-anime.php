f2534bd13fea55a930504b41f66a4540e763b73f
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-03-24 13:49:02 +0000

    Add Anime Form

---
+++ add-anime.php
@@ -0,0 +1,28 @@
+<?php require_once('header.php'); ?>
+<div class="container">
+    <form action="" method="post">
+        <div class="row">
+            <h1>Add New Anime</h1>
+        </div>
+        <div class="row">
+            <label>
+                Name: <input type="text">
+            </label>
+            <label>
+                Description: <textarea name="" id="" cols="30" rows="10"></textarea>
+            </label>
+            <label>
+                Airing?: <input type="checkbox">
+            </label>
+            <label>
+                Start Date: <input type="date" name="" id="">
+            </label>
+            <label>
+                End Date: <input type="date" name="" id="">
+            </label>
+        </div>
+        
+    </form>
+</div>
+
+<?php require_once('footer.php'); ?>
