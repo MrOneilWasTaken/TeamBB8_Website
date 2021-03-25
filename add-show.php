0f06e500e782f975f3c0a6eac67ed2255a1182a8
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-03-25 16:37:47 +0000

    Reformated the form to be more even

--- add-show.php
+++ add-show.php
@@ -1,73 +1,58 @@
 <?php require_once('header.php'); ?>
 <div class="container">
     <form id="newShowForm" action="add-anime-process.php" method="post">
-        <div class="row">
-            <h1>Add New Show</h1>
+        <div class="row text-center">
+            <div class="col-12">    
+                <h1>Add New Show</h1>
+            </div>
         </div>
-
         <div class="row">
-            <div class="col-12 col-md-4">
-                <label>
-                    Name: 
-                    <input type="text" class="full-width" name="showName" required>
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Name:</label>
+                <input type="text" class="full-width" name="showName" required>
             </div>
-            <div class="col-12 col-md-4">
-                <label>
-                    Description: 
-                    <textarea class="full-width" name="showDesc" id="" oninput="autoHeight()" required></textarea>
-                </label>
-            </div>
-            <div class="col-12 col-md-4">
-                <label>
-                    Airing?: 
-                    <input name="isAiring" id="checkboxAiring" type="checkbox">
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Description:</label>
+                <textarea class="full-width" name="showDesc" id="" oninput="autoHeight()" required></textarea>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-12 col-md-4">
-                <label>
-                    Start Date: 
-                    <input class="full-width" name="startDate" type="date" name="" id="" required>
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Start Date:</label>
+                <input class="full-width" name="startDate" type="date" name="" id="" required>
             </div>
-            <div  class="col-12 col-md-4">
-                <label id="endDateCont">
-                    End Date: 
-                    <input class="full-width" name="endDate" type="date" name="" id="endDate" required>
-                </label>
+            <div id="endDateCont" class="col-12 col-md-6">
+                <label>End Date:</label>
+                <input class="full-width" name="endDate" type="date" name="" id="endDate" required>
             </div>
         </div>
 
         <div class="row">
-            <div class="col-12 col-md-4">
-                <label>
-                    Genre: 
-                    <select class="full-width" name="showCat" id="" required>
-                        <option value="">Something</option>
-                    </select>
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Genre:</label>
+                <select class="full-width" name="showCat" id="" required>
+                    <option value="">Something</option>
+                </select>
             </div>
-            <div class="col-12 col-md-4">
-                <label>
-                    Studio:
-                    <select class="full-width" name="showStu" id="" required>
-                        <option value="">Something else</option>
-                    </select>
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Studio:</label>
+                <select class="full-width" name="showStu" id="" required>
+                    <option value="">Something else</option>
+                </select>
             </div>
         </div>
         <div class="row">
-            <div class="col-12">
-                <label>
-                    Image name:
-                    <input class="full-width" type="text" name="image" id="" required>
-                </label>
+            <div class="col-12 col-md-6">
+                <label>Image name:</label>
+                <input class="full-width" type="text" name="image" id="" required>
             </div>
-            <div class="col-12">
-                <input type="submit" value="Add Anime">
+            <div class="col-12 col-md-6">
+                <label>Airing?:</label><br/>
+                <input name="isAiring" id="checkboxAiring" type="checkbox">
+            </div>
+            <div class="col-12 text-center">
+                <input type="submit" value="Add Show">
             </div>
         </div>
     </form>
