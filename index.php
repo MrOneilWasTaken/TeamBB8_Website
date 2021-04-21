8b014ea347c2b828259ad0f9bdc56ce796918384
Daniel Washington <danwashington640@gmail.com>
2021-04-21 16:42:32 +0100

    extracting from database

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
-<?php require_once('header.php');
+<?php 
+require_once('header.php');
     
 $showList = "SELECT showImage, showName, showStu FROM shows
     INNER JOIN studio ON shows.showStu = studio.stuID";
@@ -7,14 +8,14 @@ $showList = "SELECT showImage, showName, showStu FROM shows
 
     $showPrep->execute();
 
-while($showRow = $prepShow->fetchObject()) { ?>
+while($showRow = $showPrep->fetchObject()) { ?>
 
 <div class="container-fluid" id="shows-section">
 
     <h2 id="pageTitle">Browse Shows</h2><br />
 
         <div class="row row-cols-3">
-            <div class="col-md-4"><img src="img/<?php $showRow->showImage; ?>" class="img-thumbnail" alt="<?php echo $showRow->showName; ?>"></a>
+            <div class="col-md-4"><img class="img-thumbnail" src="img/<?php $showRow->showImage; ?>" alt="<?php echo $showRow->showName; ?>"></a>
                 <div> <?php echo $showRow->showName; ?> <br />
                     <?php echo $showRow->showStu; ?> <br />
                     <button type="button" class="btn btn-dark" style="background-color: #8AAFD5;" data-toggle="modal" data-target="#showModal">
