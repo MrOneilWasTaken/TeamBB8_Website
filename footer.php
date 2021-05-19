f35074534b89536e7f1a5e77557eacbdfdbe9825
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-05-19 18:33:30 +0100

    added styles to css file and created separate js file for autocomplete

--- footer.php
+++ footer.php
@@ -13,5 +13,10 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <script src="ajax.js"></script>
 <script src="js/admin.js"></script>
+
+<script src="js/search.js"></script>
+<script>    
+    autocomplete(input, <?= json_encode($showList) ?>);
+</script>
 </body>
 </html>
